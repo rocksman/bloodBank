3dd4c5866a5f58e66071de70539ea3a0d7e2836c
rocksman <ronsam98@gmail.com>
2018-09-24 22:57:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,24 @@ require('./config/passport')(passport);
 // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser()); // get information from html forms
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://agile-mountain-57574.herokuapp.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.use(express.static('./dist/bloodAdmin')); // set up ejs for templating
 
